af096a97d777174cf3a9393508450f4ca1803e60
Rodrigo Luger <rodluger@gmail.com>
2018-10-17 15:09:42 -0400

    added image

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Cloud variability mapping on [brown dwarfs](http://starwars.wikia.com/wiki/Bespi
 pip install starry
 python bespin.py
 ```
+
+<p align="center">
+  <img width = "450" src="https://github.com/rodluger/bespin/blob/master/img/clouds.gif?raw=true"/>
+</p>
--- bespin.py
+++ bespin.py
@@ -1,6 +1,8 @@
 import starry
 import matplotlib.pyplot as pl
 import numpy as np
+import george
+from george import kernels
 np.random.seed(42)
 
 
@@ -9,12 +11,9 @@ def power(l):
     return l ** -2
 
 
-# Instantiate a map
-lmax = 10
-map = starry.Map(lmax=lmax)
-
-# Now let's draw coefficients at random
+# Let's draw coefficients at random
 # according to the simple power spectrum
+lmax = 10
 y = np.array([1.])
 for l in range(1, lmax + 1):
     p = power(l)
@@ -22,8 +21,18 @@ for l in range(1, lmax + 1):
     yl *= np.sqrt(p / np.sum(yl ** 2))
     y = np.concatenate((y, yl))
 
-# Assign the vector to the `starry` map
-map[:, :] = y
+# Add variability
+ntime = 100
+time = np.linspace(0, 10, ntime)
+amp = 1.0
+tau = 1.0
+kernel = [None] + [amp * power(l) ** 2 * kernels.ExpSquaredKernel(tau ** 2)
+                   for l in range(1, lmax + 1)]
+gp = [None] + [george.GP(kernel[l]) for l in range(1, lmax + 1)]
+y_of_t = np.concatenate([[gp[l].sample(time) for m in range(-l, l + 1)] for l in range(1, lmax + 1)])
+y_of_t = np.concatenate((np.ones_like(time).reshape(1, -1), y_of_t))
 
-# Show it
-map.animate()
+# Instantiate and plot the map
+map = starry.Map(lmax, ntime)
+map[:, :] = y.reshape(-1, 1) + y_of_t
+map.show(gif="clouds.gif", show_labels=False)

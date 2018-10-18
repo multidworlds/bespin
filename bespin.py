175b3f75794924d6ef7de739aa68f411bb4e8481
Rodrigo Luger <rodluger@gmail.com>
2018-10-18 17:02:50 -0400

    getting Bayesian

--- bespin.py
+++ bespin.py
@@ -9,17 +9,21 @@ np.random.seed(42)
 cmap = pl.get_cmap("inferno")
 
 
-def power(l, alpha=2.0, beta=-2.0, l0=2):
+def power(l, amp=0.1, alpha=2.0, beta=-2.0, l0=2):
     """A broken power law power spectrum."""
+    l0 = int(l0)
     if hasattr(l, "__len__"):
-        p = ((l + 1.0) / (l0 + 1.0)) ** alpha
-        p[l >= l0] = ((l[l >= l0] + 1.0) / (l0 + 1.0)) ** beta
+        p = amp * ((l + 1.0) / (l0 + 1.0)) ** alpha
+        p[l >= l0] = amp * ((l[l >= l0] + 1.0) / (l0 + 1.0)) ** beta
+        p[0] = 1.0
         return p
     else:
-        if l < l0:
-            return ((l + 1.0) / (l0 + 1)) ** alpha
+        if l == 0:
+            return 1.0
+        elif l < l0:
+            return amp * ((l + 1.0) / (l0 + 1)) ** alpha
         else:
-            return ((l + 1.0) / (l0 + 1)) ** beta
+            return amp * ((l + 1.0) / (l0 + 1)) ** beta
 
 
 def get_power(y):
@@ -34,22 +38,23 @@ def get_power(y):
     return p
 
 
-def variable_map(lmax=10, amp=1.0, tau=0.025, npts=300,
+def variable_map(lmax=10, amp=0.1, tau=0.025, npts=300,
                  alpha=2.0, beta=-2.0, l0=2):
     """Generate variability using a GP."""
     time = np.linspace(0.0, 1.0, npts)
     y = np.zeros(((lmax + 1) ** 2, npts))
-    n = 0
-    for l in range(0, lmax + 1):
+    y[0, :] = 1.0
+    n = 1
+    for l in range(1, lmax + 1):
         if hasattr(tau, "__len__"):
-            kernel = amp * power(l, alpha=alpha, beta=beta, l0=l0) ** 2 * \
+            kernel = (power(l, amp=amp, alpha=alpha, beta=beta, l0=l0)) ** 2 * \
                      kernels.Matern32Kernel([tau[0] ** 2,
                         (tau[1] * (2 * l + 1)) ** 2], ndim=2)
             x = np.array([[ti, m] for ti in time for m in range(-l, l + 1)])
             gp = george.GP(kernel)
             sample = gp.sample(x).reshape(-1, 2 * l + 1).transpose()
         else:
-            kernel = amp * power(l, alpha=alpha, beta=beta, l0=l0) ** 2 * \
+            kernel = (power(l, amp=amp, alpha=alpha, beta=beta, l0=l0)) ** 2 * \
                      kernels.Matern32Kernel(tau ** 2)
             gp = george.GP(kernel)
             sample = np.array([gp.sample(time) for m in range(-l, l + 1)])
@@ -113,7 +118,7 @@ def plot_phase_curve(y, nrot=0.5):
     theta = np.linspace(0, 360 * nrot, npts)
     flux = np.zeros(npts)
     fig, ax = pl.subplots(1, figsize=(8, 5))
-    for t in tqdm(range(npts)):
+    for t in range(npts):
         map[:, :] = y[:, t]
         map.rotate(theta[t])
         flux[t] = map.flux()
@@ -137,7 +142,6 @@ def animate(y, res=150, cmap="plasma", gif="", interval=75, nrot=0.5):
         for i in range(res):
             for j in range(res):
                 I[t, j, i] = map(x=x[i], y=x[j])
-
     fig, ax = pl.subplots(1, figsize=(3, 3))
     img = ax.imshow(I[0], origin="lower", interpolation="none", cmap=cmap,
                     extent=(-1, 1, -1, 1), animated=True,
@@ -150,7 +154,6 @@ def animate(y, res=150, cmap="plasma", gif="", interval=75, nrot=0.5):
 
     ani = animation.FuncAnimation(fig, updatefig, interval=interval, blit=True,
                                   frames=len(I))
-
     if gif != "":
         if gif.endswith(".gif"):
             gif = gif[:-4]
@@ -160,18 +163,83 @@ def animate(y, res=150, cmap="plasma", gif="", interval=75, nrot=0.5):
     pl.close()
 
 
-# Create a variable map
-lmax = 10
-amp = 1.0
-tau = 0.03
+def example():
+    """An example of a variable map."""
+    y = variable_map(lmax=10, amp=0.1, tau=0.1, npts=300,
+                     alpha=2.0, beta=-2.0, l0=2)
+    plot_power(y)
+    plot_coeffs(y)
+    plot_phase_curve(y)
+    animate(y, nrot=0.5)
+
+
+def phase_curve(map, time, per):
+    """Generate a rotational phase curve."""
+    theta = 360.0 / per * time
+    flux = map.flux(theta=theta)
+    return np.array(np.diag(flux))
+
+
+def lnlike(x, time, flux, err, map):
+    """Log-likelihood."""
+    # Generate the light curve from the params
+    amp, tau, alpha, beta, l0, per = x[:6]
+    map[:, :] = x[6:].reshape(-1, len(time))
+    model = phase_curve(map, time, per)
+
+    # Hard bounds
+    if (amp < 0) or (tau < 0) or (alpha < 0) or (beta > 0) or (per < 0):
+        return -np.inf
+    elif (l0 < 0) or (l0 > map.lmax):
+        return -np.inf
+    else:
+        ll = 0
+
+    # Compute the GP prior
+    for l in range(0, lmax + 1):
+        kernel = power(l, amp=amp, alpha=alpha, beta=beta, l0=l0) ** 2 * \
+                 kernels.Matern32Kernel(tau ** 2)
+        gp = george.GP(kernel)
+        gp.compute(time)
+        for m in range(-l, l + 1):
+            ll += gp.log_likelihood(y[l ** 2 + l + m, :])
+
+    # Compute the likelihood
+    ll += -0.5 * np.sum((model - flux) ** 2) / err ** 2
+
+    return ll
+
+
+def neglnlike(*args, **kwargs):
+    return -lnlike(*args, **kwargs)
+
+
+example()
+quit()
+
+
+# These are fixed
+lmax = 5
 npts = 300
-nrot = 3.0
+err = 0.01
+time = np.linspace(0.0, 1.0, npts)
+
+# Parameters we'll try to recover
+per = 0.4345
+amp = 0.01
+tau = 0.125
 alpha = 2.0
 beta = -2.0
-l0 = 2
+l0 = 1
+
+# Generate the synthetic light curve
 y = variable_map(lmax=lmax, amp=amp, tau=tau, npts=npts,
                  alpha=alpha, beta=beta, l0=l0)
-plot_power(y)
-plot_coeffs(y)
-plot_phase_curve(y)
-animate(y, nrot=nrot)
+map = starry.Map(lmax, npts)
+map[:, :] = y
+flux0 = phase_curve(map, time, per)
+flux = flux0 + err * np.random.randn(npts)
+
+x = np.array([amp, tau, alpha, beta, l0, per] + list(y.flatten()))
+print(x.shape)
+print(lnlike(x, time, flux, err, map))
